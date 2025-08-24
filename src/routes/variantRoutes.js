701f7926667fb7597d5d14d522cec6ac30f0975f
jstOnly4 <thanhvuphan2987@gmail.com>
2025-08-24 16:05:07 +0700

    fix routes for upload add middle

--- src/routes/variantRoutes.js
+++ src/routes/variantRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middlewares/upload");
 const { createVariant } = require("../controllers/variantController");
+const { authMiddleware, staffOrAdmin } = require("../middlewares/authMiddleware");
 
-router.post("/add-variant", upload.array("images", 5), createVariant);
+router.post("/add-variant", upload.array("images", 5),authMiddleware, staffOrAdmin, createVariant);
 
 module.exports = router;
