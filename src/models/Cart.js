1b29bb97107ff6fa9b392a5cc534d163acfb3a4c
jstOnly4 <thanhvuphan2987@gmail.com>
2025-10-06 16:35:08 +0700

    api for cart and uuid

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,21 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const CartItemSchema = new mongoose.Schema({
-  variantId: { type: mongoose.Schema.Types.ObjectId, ref: "ProductVariant" },
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-  name: String,
-  color: String,
-  size: String,
+const cartItemSchema = new mongoose.Schema({
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  variantId: { type: mongoose.Schema.Types.ObjectId, ref: 'ProductVariant', required: true },
+  size: { type: String, required: true },
   quantity: { type: Number, default: 1 },
-  price: Number,
-  image: String
-}, { _id: false });
+  price: Number,          // giá gốc
+  discountPrice: Number,  // giá sau giảm (nếu có)
+  finalPrice: Number      // finalPrice = discountPrice || price
+}, { _id: true });
 
-const CartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  items: [CartItemSchema],
-  totalPrice: { type: Number, default: 0 }
+const cartSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
+  guestId: { type: String, default: null },
+  items: [cartItemSchema],
+  currency: { type: String, default: 'VND' },
+  updatedAt: { type: Date, default: Date.now }
 }, { timestamps: true });
 
-module.exports = mongoose.model("Cart", CartSchema);
-    
+cartSchema.index({ userId: 1 }, { unique: true, partialFilterExpression: { userId: { $type: 'objectId' } } });
+cartSchema.index({ guestId: 1 }, { unique: true, partialFilterExpression: { guestId: { $type: 'string' } } });
+
+module.exports = mongoose.model('Cart', cartSchema);
