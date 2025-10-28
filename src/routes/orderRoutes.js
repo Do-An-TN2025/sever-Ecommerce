6c45dd89b7f1fdcfa23e455bb5effd41b57e5339
jstOnly4 <thanhvuphan2987@gmail.com>
2025-10-28 17:16:49 +0700

    statist for admin

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -11,7 +11,7 @@ const {
   updateOrderStatus
 } = require("../controllers/orderController");
 const { authMiddleware , authOptional , adminOnly } = require("../middlewares/authMiddleware");
-
+const statisController = require('../controllers/statisController');
 
 router.post("/create-orders", authOptional, createOrder);
 
@@ -22,9 +22,12 @@ router.post("/payos/webhook", handlePayOSWebhook);
 //admin routes 
 router.get("/admin", authMiddleware , adminOnly , getOrdersAdmin);
 router.patch("/admin/:id/status", authMiddleware, adminOnly, updateOrderStatus);
-  
+router.get('/stats/overview', authMiddleware , adminOnly, statisController.getAdminStats);
+router.get('/stats/sales', authMiddleware , adminOnly, statisController.getSalesByPeriod);
+router.get('/stats/top-products', authMiddleware , adminOnly, statisController.getTopProducts);
+
 router.get("/",authMiddleware,getMyOrders);
-router.get("/:id", authMiddleware, getOrderById);
+router.get("/:id", getOrderById);
 
 router.get('/payment-status/:orderCode', checkPaymentStatus);
 router.post('/:id/cancel', cancelOrder);
